2127ac3a239d6f425e3a39a97ea2dfc42b723e7c
OldGordon <grindersan@gmail.com>
2016-11-04 19:46:48 +0100

    More changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import BookList from '../containers/book_list';
 
+import BookList from '../container/book-list';
+import BookDetail from '../containers/book-detail';
 
 export default class App extends Component {
   render() {
